01a68ba16e0815246347cc81f937ca603c45a59e
chandrakala <chadrakalasv1502@gmail.com>
2022-05-21 12:44:55 +0530

    added functionality to check employee is present or absent

--- src/com/bl/emp/EmployeeWage.java
+++ src/com/bl/emp/EmployeeWage.java
@@ -5,5 +5,13 @@ import java.sql.SQLOutput;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
+        int check = (int) ((Math.random() * 10) % 2);
+        if (check == 1) {
+            System.out.println("employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
+        }
+
     }
 }
