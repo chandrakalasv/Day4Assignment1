c6a8880dbc7fccc9b7546522e67119d99d5e1105
chandrakala <chadrakalasv1502@gmail.com>
2022-05-21 12:20:10 +0530

    added code to welcome for for the employee wage computation problem

---
+++ src/com/bl/emp/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bl.emp;
+
+import java.sql.SQLOutput;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
