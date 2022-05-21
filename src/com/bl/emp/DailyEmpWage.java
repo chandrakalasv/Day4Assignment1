29b7f69474af9e86f77d10f9b296899abf84cade
chandrakala <chadrakalasv1502@gmail.com>
2022-05-21 13:43:53 +0530

    added functionality to calculate daily employee wage for full day employee

---
+++ src/com/bl/emp/DailyEmpWage.java
@@ -0,0 +1,7 @@
+package com.bl.emp;
+
+public class DailyEmpWage {
+    int dailyWage(int wage_per_hour, int hour_per_day) {
+        return (wage_per_hour * hour_per_day);
+    }
+}
--- src/com/bl/emp/EmployeeWage.java
+++ src/com/bl/emp/EmployeeWage.java
@@ -1,17 +1,17 @@
 package com.bl.emp;
 
-import java.sql.SQLOutput;
-
 public class EmployeeWage {
     public static void main(String[] args) {
+        DailyEmpWage wage = new DailyEmpWage();
         System.out.println("Welcome to employee wage computation program");
         int check = (int) ((Math.random() * 10) % 2);
         if (check == 1) {
             System.out.println("employee is present");
+            int full_time_salary = wage.dailyWage(20, 8);
+            System.out.println("full time employee daily wage" + " " + full_time_salary);
         }
         else {
             System.out.println("employee is absent");
         }
-
     }
 }
