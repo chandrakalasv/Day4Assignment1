131f0350275642b62a705cf95562de307c2a6e16
chandrakala <chadrakalasv1502@gmail.com>
2022-05-21 12:29:40 +0530

    Refactored unwanted lines

--- src/com/bl/emp/EmployeeWage.java
+++ src/com/bl/emp/EmployeeWage.java
@@ -1,7 +1,5 @@
 package com.bl.emp;
 
-import java.sql.SQLOutput;
-
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
